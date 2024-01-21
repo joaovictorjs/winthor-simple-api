a6c613a6ae52b1b1abd178aaed6f9fd5c5b84c94
joaovictorjs <skai7ami@gmail.com>
2024-01-21 09:52:49 -0300

    database connection on costumers endpoint

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <crow.h>
+#include <occi.h>
 
 int
 main(const int argc, const char** argv)
@@ -13,7 +14,17 @@ main(const int argc, const char** argv)
   crow::SimpleApp app;
 
   CROW_ROUTE(app, "/v1/costumers/<int>")
-  ([](const int code) { return std::to_string(code); });
+  ([&argv](const int code) {
+    using namespace oracle::occi;
+
+    Environment* env = Environment::createEnvironment(Environment::DEFAULT);
+    Connection* conn = env->createConnection(argv[1], argv[2], argv[3]);
+
+    env->terminateConnection(conn);
+    Environment::terminateEnvironment(env);
+
+    return std::to_string(code);
+  });
 
   CROW_ROUTE(app, "/v1/products/<int>")
   ([](const int code) { return std::to_string(code); });
